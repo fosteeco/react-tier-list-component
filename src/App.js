fc94060ff51af43ea6e1e2a67f5e8efa25db0978
Christian Foster <fosterchris000@gmail.com>
2022-03-10 09:28:06 -0500

    Created an add multiple function that can add multiple items function.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,49 @@ function App() {
   const [genres, setGenres] = useState({});
   const [itemCount, setItemCount] = useState(0);
 
+  useEffect(() => {
+    addMultipleItems(20);
+  }, []);
+
+  const addMultipleItems = (amount) => {
+    let newItems = {};
+    let newItemRow = [];
+    let currentItemCount = itemCount;
+    setItemCount(itemCount + amount);
+    for (let i = 0; i < amount; i++) {
+      const itemName = "item-" + currentItemCount;
+      console.log("itemName :", itemName);
+      const newItem = {
+        [itemName]: {
+          id: itemName,
+          altText: itemName,
+          desc: itemName,
+        },
+      };
+      const oldItemTrayRow = appState.newDraft.rows["row-tray"].itemIds;
+      newItemRow = [...newItemRow, itemName];
+      newItems = { ...newItems, ...newItem };
+      currentItemCount++;
+    }
+    setAppState((prevState) => ({
+      ...prevState,
+      newDraft: {
+        ...prevState.newDraft,
+        items: { ...newItems, ...prevState.newDraft.items },
+        rows: {
+          ...prevState.newDraft.rows,
+          "row-tray": {
+            ...prevState.newDraft.rows["row-tray"],
+            itemIds: [
+              ...prevState.newDraft.rows["row-tray"].itemIds,
+              ...newItemRow,
+            ],
+          },
+        },
+      },
+    }));
+  };
+
   const addItem = () => {
     const itemName = "item-" + itemCount;
     const newItem = {
